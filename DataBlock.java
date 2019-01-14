3cdb69e2994d5d45579e8832430b77586194c3cb
Md. Rakibul Islam <36030906+RakibulIslam1161062@users.noreply.github.com>
2019-01-15 09:19:15 +1300

    Add files via upload

--- DataBlock.java
+++ DataBlock.java
@@ -1,4 +1,3 @@
-
 public class DataBlock {
 	public int a;
 	public int b;
--- Nearest.java
+++ Nearest.java
@@ -9,127 +9,110 @@ import java.util.Scanner;
 
 
 public class Nearest {
-	
+		
+	public static int j=0;
 	public static void main(String args[]) throws IOException
 	{
 		
 		
-		int j=0,k=0,parcent=0;
-		System.out.println("lol");
-		File file= new File("C:\\Users\\Rakib bhai\\Downloads\\datafile\\new.txt");
-		BufferedReader br =new BufferedReader(new FileReader(file));
-		String st;
+		int totalData=0,k=0,parcent=0,bound;
 		DataBlock[] Db= new DataBlock[1110];
 		
-		while((st=br.readLine())!= null)
-		{
-			
-			String[] s=st.split(",");
-			//System.out.println(j+"  "+s);
-			String s1 = null,s2 = null,s3 = null,s4 = null;
-			
-			int foo1 =Integer.parseInt(s[0]);
-			int foo2 =Integer.parseInt(s[1]);
-			int foo3 =Integer.parseInt(s[2]);
-			int foo4 =Integer.parseInt(s[3]);
-			//System.out.println("foo4 here "+foo4);
-			 Db[j]=new DataBlock(foo1,foo2,foo3,foo4);
-			
-			
-			//System.out.println(Db[j].a+" "+Db[j].b+" "+Db[j].c+" "+Db[j].cl);
-			j++;	
-		}
-		/*for(int i=0;i<j;i++)
-			System.out.println(i+" "+ Db[i].a+" "+Db[i].b+" "+Db[i].c+" "+Db[i].cl+"\n");
-		*/
-		Random ran= new Random(1001);
-		int bound=j/10+1;
+		Db=dataReader();
+		totalData=j;
+		bound=totalData/10;
+		System.out.println("total "+totalData);
 		parcent=bound;
-		//System.out.println(parcent);
 		int [] num= new int[parcent];
 		
-		while( parcent>0)
-	   {
+		shuffle(Db,num,parcent,totalData);
+		knnAlgo(Db,num,bound,totalData);
+		
+		double accouracy=accouracyCheck(Db,num,bound,totalData);
+		
+		
+	    System.out.println(" Validity " +accouracy+" ");
+	}
+	
+	
+	
+	
+	public static double root(DataBlock x, DataBlock y)
+	{
+		return Math.sqrt((x.a-y.a)*(x.a-y.a)+(x.b-y.b)*(x.b-y.b)+(x.c-y.c)*(x.c-y.c));
+	}
+	
+	public static int classFinder( double[] x,double y,int totalData)
+	{
+		int l = 0;
+		for(int i=0;i<totalData;i++) {
 			
-			int n= ran.nextInt(j-1)+0;
-			if(Db[n].testCase==false) 
-			{
-				Db[n].testCase=true;
-				System.out.println("I am True "+n+" ");
-				num[k]=n;
-				k++;
-				parcent--;
+			if(x[i]==y) {
+				l= i;
+				break;
 			}
-			
 		}
+		return l;
+	}
+	
+	public static double accouracyCheck(DataBlock[] Db,int[] num,int bound,int totalData)
+	{
+		int predClass=0;
+		for(int v=0;v<bound;v++)
+			if(Db[num[v]].cl==Db[num[v]].predCl) predClass++;
+			//System.out.println(Db[num[v]].cl+"  vs pred "+Db[num[v]].predCl );
 		
-		/*for(int l=0;l<bound;l++)
-			System.out.println(num[l]+" ");*/
-		//for(int i=0;i<k;i++)
-			//System.out.println(Db[num[i]].a+"  "+i+"\n");
 		
+		//System.out.println("predClass "+predClass);
+		double validity=0.0;
+		//System.out.println(predClass+"  "+bound+"  ");
 		
-		double[] rootValue=new double[j] ;
-		double[] rootValue2=new double[j];
-		double[] topRootValue= new double[3];
+		validity=((double)predClass/bound)*100.0;
+		return validity;
+	}
+	
+	
+	public static void knnAlgo(DataBlock[] Db,int[] num,int bound,int totalData )
+	{
+		double[] distanceValue=new double[totalData] ;
+		double[] distanceValue2=new double[totalData];
+		double[] topDistanceValue= new double[3];
 		
 		//System.out.println("here j= "+j + "j- bouund=   "+ (j-parcent));
 		for(int i=0;i<bound;i++) {
-			for(int m=0;m<j;m++)
+			for(int m=0;m<totalData;m++)
 			{
 				if(Db[m].testCase==false) {
-					rootValue[m]=root(Db[num[i]],Db[m]);
-					rootValue2[m]=rootValue[m];
+					distanceValue[m]=root(Db[num[i]],Db[m]);
+					distanceValue2[m]=distanceValue[m];
 				}
 				else 
 				{
-					rootValue[m]=99999999;
-					rootValue2[m]=9999999;
+					distanceValue[m]=99999999;
+					distanceValue2[m]=9999999;
 				}
 			}
 			/*for(int v=0;v<j;v++)
 				System.out.println(rootValue2[v]);
 			*/
-			Arrays.sort(rootValue);
+			Arrays.sort(distanceValue);
 			//System.out.println(Arrays.toString(rootValue));
 			
 			int[] vote =new int[3];
 			int k1=0,k2=0,k3=0;
 			//System.out.println();
-			for(int u=0;u<j;u++)
-			{
-				if(rootValue2[u]==rootValue[0]) {
-					k1=u;
-				break;
-				}
-			}
-			for(int u=0;u<j;u++)
-			{
-				if(rootValue2[u]==rootValue[1]) {
-					k2=u;
-					break;
-				}
-				
-			}
-			for(int u=0;u<j;u++)
-			{
-				if(rootValue2[u]==rootValue[2]) {
-					k3=u;
-					break;
-				}
-				
-			}
-			/*k1=classFinder(rootValue2,rootValue[0],j);
-			k2=classFinder(rootValue2,rootValue[1],j);
-			k3=classFinder(rootValue2,rootValue[2],j);
-			System.out.println(k1+" kk "+k2+" "+k3);
-			*/
-			System.out.println(k1+" kk "+k2+" "+k3+" "+i+" ");
+			
+			k1=classFinder(distanceValue2,distanceValue[0],totalData);
+			k2=classFinder(distanceValue2,distanceValue[1],totalData);
+			k3=classFinder(distanceValue2,distanceValue[2],totalData);
+			//System.out.println(k1+" kk "+k2+" "+k3);
+			//*/
+			//System.out.println(k1+" kk "+k2+" "+k3+" "+i+" ");
 			vote[0]=Db[k1].cl;
 			vote[1]=Db[k2].cl;
 			vote[2]=Db[k3].cl;
 			
-			System.out.println("lolo" +vote[0]+"  "+vote[1]+"  "+vote[2]);
+			//System.out.println("lolo" +vote[0]+"  "+vote[1]+"  "+vote[2]);
 			int for1=0,for2=0;
 			for(int p=0;p<3;p++) if(vote[p]==1) for1++; else for2++;
 			
@@ -137,42 +120,56 @@ public class Nearest {
 			
 		}
 		
-		int predClass=0;
-		for(int v=0;v<bound;v++){
-			if(Db[num[v]].cl==Db[num[v]].predCl) predClass++;
-			System.out.println(Db[num[v]].cl+"  vs pred "+Db[num[v]].predCl );
-		
-		}
-		System.out.println("predClass "+predClass);
-		double validity=0.0;
-		//System.out.println(predClass+"  "+bound+"  ");
-		
-		validity=(predClass/31.0)*100;
-		System.out.println("yo    Validity " +validity+" parcent");
 	}
-	
-	public static double root(DataBlock x, DataBlock y)
+	public static DataBlock[] dataReader() throws NumberFormatException, IOException
 	{
-		return Math.sqrt((x.a-y.a)*(x.a-y.a)+(x.b-y.b)*(x.b-y.b)+(x.c-y.c)*(x.c-y.c));
-	}
-	
-	public static int classFinder( double[] x,double y,int j)
-	{
-		int l = 0;
-		for(int i=0;i<j;i++) {
+		
+		File file= new File("E:\\db\\data.txt");
+		BufferedReader br =new BufferedReader(new FileReader(file));
+		String st;
+		DataBlock[] Db= new DataBlock[1110];
+		while((st=br.readLine())!= null)
+		{
 			
-			//System.out.println("hmm "+x[i]+"  -"+y);
-			if(x[i]==y) {
-				//System.out.println("here + "+l+" ");
-				l= i;
-				break;
-			}
+			String[] s=st.split(",");
+			//System.out.println(j+"  "+s);
+			String s1 = null,s2 = null,s3 = null,s4 = null;
+			
+			int attribute1 =Integer.parseInt(s[0]);
+			int attribute2 =Integer.parseInt(s[1]);
+			int attribute3 =Integer.parseInt(s[2]);
+			int classs =Integer.parseInt(s[3]);
+			//System.out.println("foo4 here "+foo4);
+			 Db[j]=new DataBlock(attribute1,attribute2,attribute3,classs);
+			
+			
+			//System.out.println(Db[j].a+" "+Db[j].b+" "+Db[j].c+" "+Db[j].cl);
+			j++;
+			 //System.out.println(j);
 		}
-		//System.out.println("returned value "+l+" ");
-		return l;
+		return Db;
+		
 	}
 	
-	
-	
+	public static void shuffle(DataBlock[] Db,int[] num,int parcent,int totalData)
+	{
+		Random ran= new Random(1001);
+		
+		int k=0;
+		while( parcent>0)
+	   {
+			int n= ran.nextInt(totalData-1)+0;
+			if(Db[n].testCase==false) 
+			{
+				Db[n].testCase=true;
+				//System.out.println("I am True "+n+" ");
+				num[k]=n;
+				k++;
+				parcent--;
+			}
+			
+		}
+		
+	}
 	
 }
